be57ccd6ab2c694ce43a6dc11742338e928038f8
AD <archied5150@yahoo.com>
2023-01-19 12:23:43 -0600

    package

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const File = require('./models/File');
 
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ dest: 'uploads' }); //initialize upload function to send all files to uploads folder
 
@@ -36,13 +37,29 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 });
 
 // file download
-app.get('/file/:id', async (req, res) => {
+app.route('/file/:id').get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
   const file = await File.findById(req.params.id); // find the file in the database
+  // check the password
+  if (file.password != null) {
+    if (req.body.password == null) {
+      // render the password page if the file has a password
+      res.render('password');
+      return;
+    }
+    // if the file has a password, check it against the user input
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      // return the user to the page if the password is incorrect
+      res.render('password', { error: true });
+      return;
+    }
+  }
   file.downloadCount++; // increment the download count
   await file.save(); //
   console.log(file.downloadCount);
 
   res.download(file.path, file.originalName); // download the file at this path with this name
-});
+}
 
 app.listen(3000);
